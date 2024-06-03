7de6f54722eb1f033fd99ea8e6d75ea28e9f7968
Edson Gaspar <edsonjoaogaspar21@gmail.com>
2024-06-03 12:11:01 +0100

    mobile

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,26 @@
-import Image from "next/image";
+"use client";
+
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { useState } from "react";
 
 export default function Home() {
+  const [coords, setCoords] = useState<{ lng: number; lat: number }>({
+    lat: 0,
+    lng: 0,
+  });
+
   return (
-    <main className="h-screen w-screen relative overflow-hidden bg-[url('/images/map.png')] bg-cover bg-center">
+    <main className="h-screen w-screen sm:w-80 sm:mx-auto sm:shadow-md relative overflow-hidden bg-[url('/images/map.png')] bg-cover bg-center">
       {/* <Image src={"/images/map.png"} alt={"map"} width={600} height={600}  className="absolute aspect-square"  /> */}
       {/* top location */}
       <div className="absolute top-0 left-0 w-full p-4">
@@ -12,20 +30,38 @@ export default function Home() {
               Localização actual
             </p> */}
             <div className="flex flex-row items-center gap-2 p-2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                className="w-4 h-4 fill-slate-400"
-                fill="currentColor"
+              <button
+                onClick={() => {
+                  window.navigator.geolocation.getCurrentPosition(
+                    (evt) => {
+                      setCoords(() => ({
+                        lng: evt.coords.longitude,
+                        lat: evt.coords.latitude,
+                      }));
+                    },
+                    (err) => {
+                      console.error("Não possível obter sua localização");
+                    }
+                  );
+                }}
               >
-                <path d="M12 20.8995L16.9497 15.9497C19.6834 13.2161 19.6834 8.78392 16.9497 6.05025C14.2161 3.31658 9.78392 3.31658 7.05025 6.05025C4.31658 8.78392 4.31658 13.2161 7.05025 15.9497L12 20.8995ZM12 23.7279L5.63604 17.364C2.12132 13.8492 2.12132 8.15076 5.63604 4.63604C9.15076 1.12132 14.8492 1.12132 18.364 4.63604C21.8787 8.15076 21.8787 13.8492 18.364 17.364L12 23.7279ZM12 13C13.1046 13 14 12.1046 14 11C14 9.89543 13.1046 9 12 9C10.8954 9 10 9.89543 10 11C10 12.1046 10.8954 13 12 13ZM12 15C9.79086 15 8 13.2091 8 11C8 8.79086 9.79086 7 12 7C14.2091 7 16 8.79086 16 11C16 13.2091 14.2091 15 12 15Z" />
-              </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  className="w-4 h-4 fill-slate-400"
+                  fill="currentColor"
+                >
+                  <path d="M12 20.8995L16.9497 15.9497C19.6834 13.2161 19.6834 8.78392 16.9497 6.05025C14.2161 3.31658 9.78392 3.31658 7.05025 6.05025C4.31658 8.78392 4.31658 13.2161 7.05025 15.9497L12 20.8995ZM12 23.7279L5.63604 17.364C2.12132 13.8492 2.12132 8.15076 5.63604 4.63604C9.15076 1.12132 14.8492 1.12132 18.364 4.63604C21.8787 8.15076 21.8787 13.8492 18.364 17.364L12 23.7279ZM12 13C13.1046 13 14 12.1046 14 11C14 9.89543 13.1046 9 12 9C10.8954 9 10 9.89543 10 11C10 12.1046 10.8954 13 12 13ZM12 15C9.79086 15 8 13.2091 8 11C8 8.79086 9.79086 7 12 7C14.2091 7 16 8.79086 16 11C16 13.2091 14.2091 15 12 15Z" />
+                </svg>
+              </button>
               <input
                 type="text"
                 name="my-location"
                 id="my-location"
                 className="w-full bg-transparent border-b p-2 pb-0 pl-0 outline-none"
                 placeholder="Localização actual"
+                value={`${coords?.lat},${coords?.lng}`}
+                disabled
               />
             </div>
           </div>
@@ -45,10 +81,10 @@ export default function Home() {
               </svg>
               <input
                 type="text"
-                name="my-location"
-                id="my-location"
+                name="refresh-location"
+                id="refresh-location"
                 className="w-full bg-transparent border-b p-2 pb-0 pl-0 outline-none"
-                placeholder="Localização actual"
+                placeholder="Localização refrescamento"
               />
             </div>
           </div>
@@ -57,9 +93,39 @@ export default function Home() {
 
       {/* bottom */}
       <div className="absolute bottom-0 left-0 right-0 p-4 rounded-t-md bg-white/80 backdrop-blur">
-        <button className="block w-full px-4 py-3 bg-blue-600 rounded text-slate-50">
-          ola
-        </button>
+        <Drawer>
+          <DrawerTrigger asChild>
+            <button className="block w-full px-4 py-3 bg-blue-600 rounded text-slate-50">
+              Detalhes
+            </button>
+          </DrawerTrigger>
+          <DrawerContent className="sm:w-80 sm:mx-auto">
+            {/* <DrawerHeader>
+              <DrawerTitle>Are you absolutely sure?</DrawerTitle>
+              <DrawerDescription>
+                This action cannot be undone.
+              </DrawerDescription>
+            </DrawerHeader> */}
+            <Tabs defaultValue="account" className="w-full sm:w-80 mt-2 p-3">
+              <TabsList className="grid w-full grid-cols-2">
+                <TabsTrigger value="account">Refrescamento</TabsTrigger>
+                <TabsTrigger value="password">Trajecto</TabsTrigger>
+              </TabsList>
+              <TabsContent value="account">
+                Make changes to your account here.
+              </TabsContent>
+              <TabsContent value="password">
+                Change your password here.
+              </TabsContent>
+            </Tabs>
+
+            {/* <DrawerFooter>
+              <DrawerClose>
+                <button>aol</button>
+              </DrawerClose>
+            </DrawerFooter> */}
+          </DrawerContent>
+        </Drawer>
       </div>
     </main>
   );
