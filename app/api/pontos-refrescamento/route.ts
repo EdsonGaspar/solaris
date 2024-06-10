9fdca9bd3bad4720da8903e9b095ac7adcaaa59e
Edson Gaspar <edsonjoaogaspar21@gmail.com>
2024-06-10 13:34:18 +0100

    Feito com João Neto

--- app/api/pontos-refrescamento/route.ts
+++ app/api/pontos-refrescamento/route.ts
@@ -1,9 +1,14 @@
 // import { NextApiRequest } from "next";
 import { NextResponse } from "next/server";
-import pontos from "../../../datas.json"
+import pontos from "../../../datas.json";
+import { getAllPontos } from "@/lib/getAllPontos";
 
-function GET() {
-    return NextResponse.json(pontos, {status: 200})
+async function GET() {
+  const result = await getAllPontos();
+  if (result.sucesso) {
+    return NextResponse.json(result.pontos, { status: 200 });
+  }
+  return NextResponse.json([], { status: 200 });
 }
 
-export {GET}
+export { GET };
